54adb00b2b8cd6c187d69f91b68a33455116ab2f
Brian Wing <bwing@anaconda.com>
2025-08-18 11:53:42 -0400

    Mostly working build, saving work

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,10 @@ set "PATH=%SRC_DIR%\build\lib\qt6\bin;%PATH%"
 
 :: QT_FEATURE_webengine_system_icu has to be OFF or else icudtl.dat doesn't get installed
 :: https://github.com/qt/qtwebengine/blob/6.9.1/src/core/api/CMakeLists.txt#L186
+::
+:: Need at least ffmpeg v7.0 to unvendor on Windows.
+:: Need at least libpng v1.6.43 to unvendor on Windows.
+:: Need at least zlib v1.3.0 to unvendor on Windows.
 cmake --log-level STATUS -S"%SRC_DIR%/%PKG_NAME%" -B"%SRC_DIR%\build" -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
@@ -39,21 +43,26 @@ cmake --log-level STATUS -S"%SRC_DIR%/%PKG_NAME%" -B"%SRC_DIR%\build" -GNinja ^
     -DQT_FEATURE_webengine_system_icu=OFF ^
     -DQT_FEATURE_webengine_system_libevent=ON ^
     -DQT_FEATURE_webengine_system_libjpeg=ON ^
+    -DQT_FEATURE_webengine_system_libopenjpeg2=ON ^
     -DQT_FEATURE_webengine_system_libpci=OFF ^
-    -DQT_FEATURE_webengine_system_libpng=ON ^
+    -DQT_FEATURE_webengine_system_libpng=OFF ^
     -DQT_FEATURE_webengine_system_libtiff=ON ^
+    -DQT_FEATURE_webengine_system_libvpx=OFF ^
     -DQT_FEATURE_webengine_system_libwebp=ON ^
     -DQT_FEATURE_webengine_system_libxml=ON ^
     -DQT_FEATURE_webengine_system_libxslt=ON ^
-    -DQT_FEATURE_webengine_system_minizip=ON ^
+    -DQT_FEATURE_webengine_system_minizip=OFF ^
     -DQT_FEATURE_webengine_system_opus=OFF ^
     -DQT_FEATURE_webengine_system_snappy=OFF ^
-    -DQT_FEATURE_webengine_system_zlib=ON
-
+    -DQT_FEATURE_webengine_system_ssl=OFF ^
+    -DQT_FEATURE_webengine_system_zlib=OFF
 if errorlevel 1 exit 1
 
-cmake --build build --target install -j %CPU_COUNT%
+cmake --build build --target install --config Release -j %CPU_COUNT%
 if errorlevel 1 exit 1
 
-xcopy /y /s %LIBRARY_PREFIX%\lib\qt6\bin\*.dll %LIBRARY_PREFIX%\bin
+xcopy /y /s %LIBRARY_PREFIX%\lib\qt6\bin\Qt6Pdf*.dll %LIBRARY_PREFIX%\bin
+if errorlevel 1 exit 1
+
+xcopy /y /s %LIBRARY_PREFIX%\lib\qt6\bin\Qt6WebEngine*.dll %LIBRARY_PREFIX%\bin
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -7,13 +7,12 @@ if [[ "${target_platform}" == linux-* ]]; then
     ${CMAKE_ARGS}
     -DQT_FEATURE_webengine_ozone_x11=ON
     -DQT_FEATURE_webengine_system_alsa=ON
-    -DQT_FEATURE_webengine_system_minizip=ON
   "
 
   if [[ "${target_platform}" == linux-aarch64 ]]; then
     # Somehow a path for /usr/include/freetype2 is still making it into the CMake PkgConfig::FREETYPE IMPORT target.
     #
-    # Webenginedriver is just used for tests and does not link against zlib correctly on linux-aarch64.
+    # Webenginedriver is just used for tests and does not link against vendored zlib correctly.
     #
     # Chromium vendors minigbm, a discontinued Intel project, which uses compiler features not compatible with our
     # aarch64 gcc compiler.
@@ -35,18 +34,11 @@ if [[ "${target_platform}" == linux-* ]]; then
     "
   fi
 
-  # IMPORTANT: Chromium didn't add flags to unvendor protobuf until very recently and not even the latest Qt 6.9.1
-  # release has them included yet. We can't fix it until we upgrade Qt versions to maybe 6.10. That is the reason why
-  # we're removing these headers and we should be able to stop as soon as Chromium provides a build option to
-  # use_system_protobuf=1.
-
-  # We can't add ${PREFIX}/include to the include paths because the conda protobuf and unicode headers will get used
-  # instead of the vendored ones so we remove the conda ones to avoid conflicts.
-  rm -rf ${PREFIX}/include/google/protobuf
-  rm -rf ${PREFIX}/include/unicode
-  rm -rf ${PREFIX}/include/openssl
-  rm -rf ${PREFIX}/include/absl
+  # Hack to help the generator executables, such as `v8_context_snapshot_generator`, that get called during a build
+  # find unvendored libraries in our host prefix.
+  export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH}"
 else
+  # Webenginedriver is just used for tests and does not link against vendored zlib correctly.
   CMAKE_ARGS="
     ${CMAKE_ARGS}
     -DQT_FORCE_WARN_APPLE_SDK_AND_XCODE_CHECK=ON
@@ -54,11 +46,40 @@ else
     -DQT_MAC_SDK_VERSION=${OSX_SDK_VER}
     -DQT_FEATURE_webengine_system_freetype=ON
     -DQT_FEATURE_webengine_system_gbm=OFF
-    -DQT_FEATURE_webengine_system_minizip=ON
+    -DQT_FEATURE_webenginedriver=OFF
   "
 fi
 
+# IMPORTANT: Chromium didn't add flags to unvendor protobuf until very recently and not even the latest Qt 6.9.1
+# release has them included yet. We can't fix it until we upgrade Qt versions to maybe 6.10. That is the reason why
+# we're removing these headers and we should be able to stop as soon as Chromium provides a build option to
+# use_system_protobuf=1.
+#
+# We can't add ${PREFIX}/include to the include paths because our protobuf, unicode, openssl, and abseil packages are
+# both lib and development packages and the included headers will get used instead of the vendored ones. To resolve
+# this, we remove the conda ones to avoid conflicts but really these should be split into devel and lib packages so
+# the unwanted headers are never even installed.
+rm -rf ${PREFIX}/include/google/protobuf
+rm -rf ${PREFIX}/include/unicode
+rm -rf ${PREFIX}/include/openssl
+rm -rf ${PREFIX}/include/absl
+rm -rf ${PREFIX}/include/vulkan
+rm -rf ${PREFIX}/include/zlib.h
+rm -rf ${PREFIX}/include/jconfig.h
+rm -rf ${PREFIX}/include/jerror.h
+rm -rf ${PREFIX}/include/jmorecfg.h
+rm -rf ${PREFIX}/include/jpeglib.h
+
+# TODO: Try to unbundle nasm and absl as well.
+
 # QT_FEATURE_webengine_system_icu has to be OFF or else icudtl.dat doesn't get installed
+# https://github.com/qt/qtwebengine/blob/6.9.1/src/core/api/CMakeLists.txt#L186
+#
+# Need at least ffmpeg v7.0 to unvendor on Unix.
+# Need at least libpng v1.6.43 to unvendor on Unix.
+# Need at least zlib v1.3.0 to unvendor on Unix.
+#
+# Both minizip and zlib have to be ON to use system minizip.
 cmake --log-level STATUS -S"${SRC_DIR}/${PKG_NAME}" -Bbuild -GNinja ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
@@ -89,24 +110,27 @@ cmake --log-level STATUS -S"${SRC_DIR}/${PKG_NAME}" -Bbuild -GNinja ${CMAKE_ARGS
   -DQT_FEATURE_webengine_qt_libjpeg=OFF \
   -DQT_FEATURE_webengine_qt_libpng=OFF \
   -DQT_FEATURE_webengine_qt_zlib=OFF \
-  -DQT_FEATURE_webengine_system_ffmpeg=ON \
+  -DQT_FEATURE_webengine_system_ffmpeg=OFF \
   -DQT_FEATURE_webengine_system_glib=ON \
   -DQT_FEATURE_webengine_system_harfbuzz=ON \
   -DQT_FEATURE_webengine_system_icu=OFF \
   -DQT_FEATURE_webengine_system_libevent=ON \
-  -DQT_FEATURE_webengine_system_libjpeg=ON \
+  -DQT_FEATURE_webengine_system_libjpeg=OFF \
+  -DQT_FEATURE_webengine_system_libopenjpeg2=ON \
   -DQT_FEATURE_webengine_system_libpci=OFF \
-  -DQT_FEATURE_webengine_system_libpng=ON \
+  -DQT_FEATURE_webengine_system_libpng=OFF \
   -DQT_FEATURE_webengine_system_libtiff=ON \
+  -DQT_FEATURE_webengine_system_libvpx=ON \
   -DQT_FEATURE_webengine_system_libwebp=ON \
   -DQT_FEATURE_webengine_system_libxml=ON \
   -DQT_FEATURE_webengine_system_libxslt=ON \
-  -DQT_FEATURE_webengine_system_minizip=ON \
+  -DQT_FEATURE_webengine_system_minizip=OFF \
   -DQT_FEATURE_webengine_system_opus=ON \
   -DQT_FEATURE_webengine_system_snappy=OFF \
-  -DQT_FEATURE_webengine_system_zlib=ON
+  -DQT_FEATURE_webengine_system_ssl=OFF \
+  -DQT_FEATURE_webengine_system_zlib=OFF
 
-cmake --build build --target install -j${CPU_COUNT}
+cmake --build build --target install --config Release -j${CPU_COUNT}
 
 pushd "${PREFIX}"
 
