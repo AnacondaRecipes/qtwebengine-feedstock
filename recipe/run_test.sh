99df6cda65b8a587677d159dd0f048ab58b210ef
Andrii-Osipov <aosipov@anaconda.com>
2026-02-10 06:10:31 -0800

    qtwebengine 6.10.1

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -12,7 +12,6 @@ export PATH=${PWD}:${PATH}
 # Keep QMake from trying to detect the SDK in the wrong place in CI.
 export QT_MAC_SDK_NO_VERSION_CHECK=1
 
-# Only test that this builds: -d for debug, -Wall for warnings, -Wparser for parser warnings
 qmake6 -Wall -Wparser qtwebengine.pro
 make
 
